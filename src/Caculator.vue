403dd1d72d1111f03094100e9ba8621397f66678
Dinh Van Hai <gemini.wind285@gmail.com>
2018-05-10 18:26:27 +0700

    Use CaculatorBtn

--- src/Caculator.vue
+++ src/Caculator.vue
@@ -11,76 +11,44 @@
             </div>
             <div class="caculator-context">
                 <el-row :gutter="15">
-                    <el-col :span="6">
-                        <el-button plain @click="handleInput('add')">+</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                        <el-button plain @click="handleInput('substract')">-</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                        <el-button plain @click="handleInput('multiple')">x</el-button>
-                    </el-col>
-                     <el-col :span="6">
-                        <el-button plain @click="handleInput('divide')">/</el-button>
-                    </el-col>
+                  <CaculatorBtn btnValue="add" btnLabel="+" span=6 @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="substract" btnLabel="-" span=6 @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="multiple" btnLabel="x" span=6 @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="divide" btnLabel="/" span=6 @press="handleInput($event)"></CaculatorBtn>
                 </el-row>
                 <el-row :gutter="15">
-                    <el-col :span="6">
-                        <el-button plain @click="handleInput('7')">7</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                          <el-button plain @click="handleInput('8')">8</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                         <el-button plain @click="handleInput('9')">9</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                            <el-button style="background-color: #f4a064" plain @click="handleInput('AC')">AC</el-button>
-                    </el-col>
+                  <CaculatorBtn btnValue="7" span=6 @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="8" span=6  @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="9" span=6 @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="AC" span=6 @press="handleInput($event)"></CaculatorBtn>
                 </el-row>
                 <el-row :gutter="15">
-                    <el-col :span="6">
-                        <el-button plain @click="handleInput('4')">4</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                          <el-button plain @click="handleInput('5')">5</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                         <el-button plain @click="handleInput('6')">6</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                         <el-button plain style="background-color: #f4a064" @click="handleInput('DEL')">DEL</el-button>
-                    </el-col>
+                  <CaculatorBtn btnValue="4" span=6 @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="5" span=6 @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="6" span=6 @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="DEL" span=6 @press="handleInput($event)"></CaculatorBtn>
                 </el-row>
                 <el-row :gutter="15">
-                    <el-col :span="6">
-                        <el-button plain @click="handleInput('1')">1</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                          <el-button plain @click="handleInput('2')">2</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                         <el-button plain @click="handleInput('3')">3</el-button>
-                    </el-col>
+                  <CaculatorBtn btnValue="1" span=6 @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="2" span=6 @press="handleInput($event)"></CaculatorBtn>
+                  <CaculatorBtn btnValue="3" span=6 @press="handleInput($event)"></CaculatorBtn>
                 </el-row>
                 <el-row :gutter="15">
-                    <el-col :span="6">
-                        <el-button plain @click="handleInput('0')">0</el-button>
-                    </el-col>
-                    <el-col :span="6">
-                          <el-button plain @click="handleInput('.')">.</el-button>
-                    </el-col>
-                    <el-col :span="12">
-                            <el-button class="btn-red-light" plain @click="handleInput('=')">=</el-button>
-                    </el-col>
+                    <CaculatorBtn btnValue="0" span=6 @press="handleInput($event)"></CaculatorBtn>
+                    <CaculatorBtn btnValue="." span=6 @press="handleInput($event)"></CaculatorBtn>
+                    <CaculatorBtn btnValue="=" span=12 @press="handleInput($event)"></CaculatorBtn>
                 </el-row>
             </div>
     </div>
-
 </template>
 
 <script>
+import CaculatorBtn from "./CaculatorBtn";
+
 export default {
+  components: {
+    CaculatorBtn
+  },
   data() {
     return {
       firstNumber: undefined,
@@ -88,7 +56,7 @@ export default {
       operator: undefined,
       result: undefined,
       tmpVal: undefined,
-      displayContext: undefined,
+      displayContext: undefined
     };
   },
   methods: {
@@ -162,17 +130,17 @@ export default {
             // then assign last result to first number
             // and continue to caculated
             if (isOperator(character)) {
-                this.firstNumber = this.result;
-                this.secondNumber = undefined;
-                this.operator = undefined;
-                this.result = undefined;
-                this.tmpVal = undefined;
-                this.setDisplayContext(this.firstNumber);
+              this.firstNumber = this.result;
+              this.secondNumber = undefined;
+              this.operator = undefined;
+              this.result = undefined;
+              this.tmpVal = undefined;
+              this.setDisplayContext(this.firstNumber);
             } else {
               // if new number is inputted
               // reset caculator context
               // then start new caculation
-                this.resetContext();
+              this.resetContext();
             }
           }
 
@@ -197,7 +165,7 @@ export default {
       }
     },
     setDisplayContext(displayContext) {
-        this.displayContext = displayContext
+      this.displayContext = displayContext;
     },
     resetContext() {
       this.firstNumber = undefined;
@@ -210,12 +178,14 @@ export default {
     // use tmp variable to store input number
     getInputNumber(character) {
       if (this.tmpVal) {
-        if (!(this.tmpVal.includes('.') && character === '.')) {
-         this.tmpVal =
-            this.tmpVal = (character === 'DEL') ? this.tmpVal.substring(0, this.tmpVal.length - 1) : this.tmpVal.concat(character);
+        if (!(this.tmpVal.includes(".") && character === ".")) {
+          this.tmpVal = this.tmpVal =
+            character === "DEL"
+              ? this.tmpVal.substring(0, this.tmpVal.length - 1)
+              : this.tmpVal.concat(character);
         }
       } else {
-        this.tmpVal = (character !== 'DEL') ? character : undefined;
+        this.tmpVal = character !== "DEL" ? character : undefined;
       }
 
       const outputNumber = this.parseInputToNumber(this.tmpVal);
@@ -227,26 +197,26 @@ export default {
 </script>
 
 <style>
-  .el-row {
-    margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
+.el-row {
+  margin-bottom: 20px;
+  &:last-child {
+    margin-bottom: 0;
   }
-  .el-col {
-    border-radius: 4px;
-  }
-  .el-button {
-    width: 100%;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .btn-red-light {
-    background-color:#e86f19;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
-  }
+}
+.el-col {
+  border-radius: 4px;
+}
+.el-button {
+  width: 100%;
+}
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.btn-red-light {
+  background-color: #e86f19;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
 </style>
---
+++ src/CaculatorBtn.vue
@@ -0,0 +1,16 @@
+<template>
+  <el-col :span="span">
+    <el-button plain @click="press(btnValue)">{{btnLabel || btnValue}}</el-button>
+  </el-col>
+</template>
+
+<script>
+export default {
+  props: ["btnValue", "span", "btnLabel", "color"],
+  methods: {
+    press(btnValue) {
+       this.$emit('press', this.btnValue)
+    }
+  }
+}
+</script>
